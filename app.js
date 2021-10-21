ed71fa1d707c006ac8f507116100c78e01cc1a85
BISINIGISHIVANI <90177603+BISINIGISHIVANI@users.noreply.github.com>
2021-10-21 19:04:00 +0530

    updated url

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
-var translateButton=document.querySelector("#translate-button");
+var btnPrimary=document.querySelector("#translate-button");
 var translateInput=document.querySelector("#translate-input");
 var translateOutput=document.querySelector("#translate-output");
-translateButton.addEventListener("click",clickHandler());
-console.log(translateInput)
+
+var serverURL="https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(input){
+    return serverURL+"?text="+input;
+}
+
+function errorHandler(error){
+    console.log("error occured",error);
+
+}
 
 
-// fetch(url)
-//     .then(response => response.json())
-//     .then(json => console.log(json))
+function clickHandler(){
+    var inputData=translateInput.value;
+    fetch(getTranslationURL(inputData))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText=json.contents.translated;
+        translateOutput.innerText=translatedText;
+    });
+   
+}
+btnPrimary.addEventListener("click",clickHandler);
 
-function clickHandler() {
-        console.log("clickd");
-        console.log("input",translateInput.value);
-};
 
-// translateButton.addEventListener("click",clickHandler());
 
